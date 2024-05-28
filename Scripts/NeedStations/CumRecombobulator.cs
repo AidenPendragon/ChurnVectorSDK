c823592fdbf9de3dcf0870f3968e3204dd617f81
stiderspace <blaze.atome@gmail.com>
2024-05-29 01:27:08 +0200

    Update CumRecombobulator.cs

--- Scripts/NeedStations/CumRecombobulator.cs
+++ Scripts/NeedStations/CumRecombobulator.cs
@@ -5,8 +5,8 @@ public class CumRecombobulator : NeedStation {
     
     public override bool ShouldInteract(CharacterBase from) {
         foreach (var condom in Condom.GetCondoms()) {
-            float distance = Vector3.Distance(condom.transform.position, transform.position);
-            if (distance < 10f) return base.ShouldInteract(from);
+            float distance = Vector3.Distance(condom.transform.position, recombobulateLocation.position);
+            if (distance < 2.5f) return base.ShouldInteract(from);
         }
         return false;
     }
@@ -14,8 +14,8 @@ public class CumRecombobulator : NeedStation {
     protected override void Activate() {
         base.Activate();
         foreach (var condom in Condom.GetCondoms()) {
-            float distance = Vector3.Distance(condom.transform.position, transform.position);
-            if (!(distance < 10f)) continue;
+            float distance = Vector3.Distance(condom.transform.position, recombobulateLocation.position);
+            if (!(distance < 2.5f)) continue;
             var churnable = condom.GetChurnable();
             if (churnable == null) {
                 Destroy(condom.gameObject);
